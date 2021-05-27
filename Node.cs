903435a87781631ba102fe07ef3faa8b427dd8d4
unknown <jimpidas>
2021-05-27 19:28:48 +0530

    [Jyosmita]Added:Creating a simple linked list

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListImplementation
+{
+    class Node
+    {
+       public int data;
+       public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
